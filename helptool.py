f9215663474fca67be51d2fc89cb5db81bc6b9f9
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2023-05-31 17:36:08 +0300

    Update helptool.py

--- helptool.py
+++ helptool.py
@@ -4,8 +4,13 @@ class HelpTool():
 1) Port scan
 2) Directory search
 3) SSH Login
-4) Search explit
-5) Exit
+4) SSH Brute Force
+5) Search exploit
+6) View Exif data
+7) Enum4linux (Get info about target)
+8) Hping3 (Dos - DDos)
+
+99) Exit
 """
 
         self.portScan = """
@@ -28,11 +33,31 @@ SSH Login tools: ssh
 ssh usage: ssh <Username>@<Server IP>  |  ssh lessy@127.0.0.1
 """
 
+        self.sshBruteForce = """
+SSH Burte force tools: Hydra
+hydra usage: hydra -l <username> -P <path to wordlist> <IP> ssh  |  hydra -l admin -P wordlist.txt 127.0.0.1 ssh              
+"""
+
         self.searchExploit = """
 Search exploit tools: searchsploit 
 searchsploit usage: searchsploit apache 2.4.49
 Also you need to update searchsploit, for update > searchsploit -u
 """
+
+        self.viewExifData = """
+Exif data viewer tools: exiftool, strings
+exiftool usage: exiftool image.png
+strings usage: string image.png        
+"""
+
+        self.enum4linux = """
+enum4linux usage: enum4linux -a 127.0.0.1        
+"""
+
+        self.hping3 = """
+hping3 usage: hping3 -S --flood -V 127.0.0.1
+Another dos - ddos tools: Slowloris, Hulk, HOIC, LOIC, Tor's Hammer
+        """
 
         self.run()
 
@@ -47,8 +72,18 @@ Also you need to update searchsploit, for update > searchsploit -u
             case "3":
                 print(self.sshLogin)
             case "4":
+                print(self.sshBruteForce)
+            case "5":
                 print(self.searchExploit)
-            case "5":  
+            case "6":
+                print(self.viewExifData)
+            case "7":
+                print(self.enum4linux)
+            case "8":
+                print(self.hping3)
+            
+            
+            case "99":  
                 exit()
 
 
